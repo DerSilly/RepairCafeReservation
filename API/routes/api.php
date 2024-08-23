e46cd5a8d7486d5265bd48137885af3309f441f8
SillyApps <dersilly@googlemail.com>
2024-08-23 13:35:16 +0200

    chore: Add ApiResponses trait and implement login endpoint

--- API/routes/api.php
+++ API/routes/api.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController; // Add this line to import the AuthController class
 
-Route::get ('/', function () {
-    return response()->json(['message' => 'Hello World!']);
-});
+Route::get ('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
