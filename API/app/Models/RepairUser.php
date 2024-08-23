54da33cd0698ead4b9b45f7b36d30a71d95bc30d
SillyApps <dersilly@googlemail.com>
2024-08-23 16:04:07 +0200

    introduced repairUser

---
+++ API/app/Models/RepairUser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\User as BaseUser;
+
+class RepairUser extends BaseUser
+{
+    // Neue Eigenschaften oder Methoden hier hinzufügen
+}
+
